0cff205bbd19f24a668df580c3557186327299fd
SIELA <502605617@qq.com>
2017-06-08 16:34:48 +0800

    scrapy+mongodb
    代码贡献者：
    scrapy内少量脑残代码：三哥
    高科技解密获取知乎具体信息：英明君

---
+++ zhihuSpider/items.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class ZhihuspiderItem(scrapy.Item):
+    # define the fields for your item here like:
+#   uid = scrapy.Field()
+#   name = scrapy.Field()
+#   now = scrapy.Field()
+#    edu = scrapy.Field()
+#    work = scrapy.Field()
+#    company = scrapy.Field()
+#    livein = scrapy.Field()
+    
+    pass
